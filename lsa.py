328ed55a60b4193cfdcaec7ba8198286f14c7719
vincent <paintart.lin@gmail.com>
2021-01-17 16:46:23 +0800

    Compute W matrix

--- lsa.py
+++ lsa.py
@@ -1,5 +1,18 @@
 import numpy as np
+import argparse
 
-class LSA:
-    def __init__(self, vocab_map, documents):
-        self.W = np.zeros(len)
+from utility import *
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('vocabulary_path', type=str, help='Input vocabulary file')
+    parser.add_argument('document_path', type=str, help='Input document file')
+    parser.add_argument('--sample_size', type=int, default=5, help='Sample size')
+    args = parser.parse_args()
+
+    vocab_map, t = build_vocabulary_map(args.vocabulary_path)
+    documents = read_documents(args.document_path)
+
+    W = build_document_matrix(vocab_map, documents, t)
+    np.save('W', W)
+
--- utility.py
+++ utility.py
@@ -1,21 +1,65 @@
+import math
+import numpy as np
 
 def build_vocabulary_map(vocabulary_path):
     print("Building vocabulary map...")
-    vacab_map = {}
+    vocab_map = {}
+    t = []
     with open(vocabulary_path, "r") as f:
         i = 0
         while True:
             line = f.readline().split()
             if not line:
                 break
-            vacab_map[line[0]] = i, line[1]
+            vocab_map[line[0]] = i
+            t.append(float(line[1]), line[0])
             i += 1
         f.close()
-    return vacab_map
+    print("[+] Number of words:", len(vocab_map))
+    return vocab_map, t
 
 def read_documents(document_path):
+    print("Rading documents...")
     documents = []
     f = open(document_path, "r")
     documents = f.readlines()
     f.close()
+    print("[+] Number of documents:", len(documents))
     return documents
+
+def compute_normalized_entropy(W, M, N, t):
+    e = []
+    r = -1.0 / math.log(N)
+    for i in range(M):
+        s = 0.0
+        for j in range(N):
+            p = float(W[i][j]) / t[i][0]
+            s += (p * math.log(p)) if p > 0.0001 else 0.0
+        e.append(r * s)
+    return e
+
+def build_document_matrix(vocab_map, documents, t):
+    print("Building matrix W...")
+    M = len(vocab_map)
+    N = len(documents)
+    W = np.zeros((M, N))
+    document_length = []
+    print("Computing c_ij")
+    # compute c_ij
+    for i, d in enumerate(documents):
+        words = d.split()
+        length = 0
+        for w in words:
+            if w in vocab_map:
+                W[vocab_map[w]][i] += 1
+                length += 1
+        document_length.append(length)
+    # normalize to word entropy
+    print("Compute entropy...")
+    e = compute_normalized_entropy(W, M, N, t)
+    print("Normalizing...")
+    for i in range(M):
+        for j in range(N):
+            W[i][j] = (1-e[i]) * W[i][j] / document_length[j]
+    print("[+] Completed")
+    return W
